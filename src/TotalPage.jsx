c3b8e355825def7f6afe1866e14d3f5d9437a6a9
Fuchen Shi <44103628+shifuchen98@users.noreply.github.com>
2019-10-24 22:09:47 -0700

    refactor: Total Score

--- src/TotalPage.jsx
+++ src/TotalPage.jsx
@@ -10,10 +10,12 @@ export default class TotalPage extends React.Component {
     const { match } = this.props;
     this.state = {
       event: AV.Object.createWithoutData('Event', match.params.id),
-      scores: []
+      eventTeams: [],
+      judgeTeamPairs: []
     };
     this.fetchEvent = this.fetchEvent.bind(this);
-    this.fetchScores = this.fetchScores.bind(this);
+    this.fetchEventTeams = this.fetchEventTeams.bind(this);
+    this.fetchJudgeTeamPairs = this.fetchJudgeTeamPairs.bind(this);
   }
 
   componentDidMount() {
@@ -30,34 +32,40 @@ export default class TotalPage extends React.Component {
     event
       .fetch()
       .then(event => {
-        this.setState({ event }, this.fetchScores);
+        this.setState({ event }, this.fetchEventTeams);
       })
       .catch(error => {
         alert(error);
       });
   }
 
-  fetchScores() {
+  fetchEventTeams() {
     const { event } = this.state;
     const eventTeamsQuery = new AV.Query('EventTeam');
     eventTeamsQuery
       .equalTo('event', event)
       .find()
       .then(eventTeams => {
-        eventTeams.forEach(eventTeam => {
-          const judgeTeamPairsQuery = new AV.Query('JudgeTeamPair');
-          judgeTeamPairsQuery
-            .equalTo('eventTeam', eventTeam)
-            .include('eventJudge')
-            .include('eventJudge.user')
-            .find()
-            .then(judgeTeamPairs => {
-              this.setState({ scores: [...this.state.scores, { eventTeam, judgeTeamPairs }] });
-            })
-            .catch(error => {
-              alert(error);
-            });
-        });
+        this.setState({ eventTeams }, this.fetchJudgeTeamPairs);
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+
+  fetchJudgeTeamPairs() {
+    const { event } = this.state;
+    const eventTeamsQuery = new AV.Query('EventTeam');
+    eventTeamsQuery
+      .equalTo('event', event);
+    const judgeTeamPairsQuery = new AV.Query('JudgeTeamPair');
+    judgeTeamPairsQuery
+      .matchesQuery('eventTeam', eventTeamsQuery)
+      .include('eventJudge')
+      .include('eventJudge.user')
+      .find()
+      .then(judgeTeamPairs => {
+        this.setState({ judgeTeamPairs });
       })
       .catch(error => {
         alert(error);
@@ -65,48 +73,34 @@ export default class TotalPage extends React.Component {
   }
 
   render() {
-    const { event, scores } = this.state;
+    const { event, eventTeams, judgeTeamPairs } = this.state;
     return (
       <div id="page">
         <div className="columns">
           <div className="column">
-            {scores.map(score =>
-              <div className="card" key={score.eventTeam.id}>
+            {eventTeams.map(eventTeam =>
+              <div className="card" key={eventTeam.id}>
                 <section className="fields">
-                  <h1>{score.eventTeam.get('name')}</h1>
+                  <h1>{eventTeam.get('name')}</h1>
                   <div className="field">
                     <table>
                       <thead>
                         <tr>
                           <th>Judge</th>
-                          {event.get('criteria').map((criterion, index) =>
-                            <th key={index}>{criterion.name} ({criterion.max})</th>
+                          {event.get('criteria').map(criterion =>
+                            <th key={criterion.name}>{criterion.name} ({criterion.max})</th>
                           )}
                           <th>Total</th>
                         </tr>
                       </thead>
                       <tbody>
-                        {score.judgeTeamPairs.map(judgeTeamPair =>
-                          <tr key={judgeTeamPair.get('eventJudge').id}>
+                        {judgeTeamPairs.filter(judgeTeamPair => judgeTeamPair.get('eventTeam').id === eventTeam.id).map(judgeTeamPair =>
+                          <tr key={judgeTeamPair.id}>
                             <td>{judgeTeamPair.get('eventJudge').get('user').get('name')}</td>
-                            {event.get('criteria').map((criterion, index) =>
-                              <td key={index}>{judgeTeamPair.get('scores').reduce((accumulator, currentValue) => {
-                                let result = accumulator;
-                                if (currentValue.name === criterion.name) {
-                                  result += currentValue.value;
-                                }
-                                return result;
-                              }, null)}</td>
+                            {event.get('criteria').map(criterion =>
+                              <td key={criterion.name}>{judgeTeamPair.get('scores').reduce((accumulator, score) => score.name === criterion.name ? accumulator + score.value : accumulator, null)}</td>
                             )}
-                            <td>{judgeTeamPair.get('scores').reduce((accumulator, currentValue) => {
-                              let result = accumulator;
-                              event.get('criteria').forEach(criterion => {
-                                if (currentValue.name === criterion.name) {
-                                  result += currentValue.value;
-                                }
-                              });
-                              return result;
-                            }, null)}</td>
+                            <td>{event.get('criteria').reduce((accumulator, criterion) => accumulator + judgeTeamPair.get('scores').reduce((accumulator, score) => score.name === criterion.name ? accumulator + score.value : accumulator, null), 0)}</td>
                           </tr>
                         )}
                       </tbody>
