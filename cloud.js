4f02bd254e55991cea62d2de96f60d562919b150
Fuchen Shi <44103628+shifuchen98@users.noreply.github.com>
2019-10-29 16:40:20 -0500

    fix: prevent non-admins from editing judges

--- cloud.js
+++ cloud.js
@@ -1,7 +1,16 @@
 const AV = require("leanengine");
-AV.Cloud.define('updateUser', request => {
-  return request.params.user
-    .set('name', request.params.name)
-    .set('email', request.params.email)
-    .save();
+AV.Cloud.define('updateUser', async request => {
+  if (request.currentUser) {
+    const roles = await request.currentUser.getRoles();
+    if (roles.filter(role => role.get('name') === 'Admin').length) {
+      return request.params.user
+        .set('name', request.params.name)
+        .set('email', request.params.email)
+        .save();
+    } else {
+      throw new AV.Cloud.Error('You are not an admin.');
+    }
+  } else {
+    throw new AV.Cloud.Error('You are not logged in.');
+  }
 });
