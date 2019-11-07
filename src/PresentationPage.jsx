427b2f2deac9ed203151916300e6ab46ead27be3
Leonardo Sanchez <leonars2@uci.edu>
2019-11-06 17:05:06 -0800

    Added a drop down menu for Presentation Page

--- src/PresentationPage.jsx
+++ src/PresentationPage.jsx
@@ -8,7 +8,11 @@ export default class PresentationPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      current: "",
+      eventTeams: []
     };
+    this.fetchEventTeams = this.fetchEventTeams.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -16,19 +20,97 @@ export default class PresentationPage extends React.Component {
     if (!AV.User.current()) {
       history.push('/');
     } else {
+      this.fetchEventTeams()
 
     }
   }
 
+  fetchEventTeams() {
+    const { match } = this.props;
+    const eventTeamsQuery = new AV.Query('EventTeam');
+    eventTeamsQuery
+      .equalTo('event', AV.Object.createWithoutData('Event', match.params.id))
+      .find()
+      .then(eventTeams => {
+        this.setState({ eventTeams });
+      })
+      .catch(error => {
+        alert(error);
+      });
+  }
+
+  handleChange(event) {
+    this.setState({ current: event.target.value })
+  }
+
   render() {
-    const { } = this.state;
+    const { eventTeams, current } = this.state;
     return (
       <div id="page">
         <div className="columns">
           <div className="column">
             <div className="card">
               <section className="fields">
-                <h1>Presentation</h1>
+                <h1>Select Rank or Team</h1>
+                <select value={current} onChange={this.handleChange}>
+                  <option value="">Rank</option>
+                  {eventTeams.map(eventTeam => <option key={eventTeam.id} value={eventTeam.get("name")}>{eventTeam.get("name")}</option>)}
+                </select>
+                {current === ""
+                  ? (
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Rank#</th>
+                          <th>Team Name</th>
+                          <th>Team Score</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Team A</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>2</td>
+                          <td>Team B</td>
+                          <td>9</td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td>Team C</td>
+                          <td>8</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  )
+                  : (
+                    <table>
+                      <thead>
+                        <tr>
+                          <th>Judge</th>
+                          <th>Score</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>Judge A</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>Judge B</td>
+                          <td>10</td>
+                        </tr>
+                        <tr>
+                          <td>Judge C</td>
+                          <td>10</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  )
+                }
+
               </section>
             </div>
           </div>
